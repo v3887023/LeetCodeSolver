db0e6ccafdfddc97bdd29c714e784f997a01a8bc
zhangcx <1720402825@qq.com>
2020-07-20 00:22:02 +0800

    增加今天要完成的3道题

---
+++ src/com/vic/solution/unsolved/P160.kt
@@ -0,0 +1,25 @@
+package com.vic.solution.unsolved
+
+import com.vic.solution.ListNode
+
+/**
+ * 160. 相交链表
+ * 编写一个程序，找到两个单链表相交的起始节点。
+ *
+ * （由于图无法显示，题目描述也简单易懂，所以就不贴示例了）
+ *
+ * 【注意】
+ * 如果两个链表没有交点，返回 null.
+ * 在返回结果后，两个链表仍须保持原有的结构。
+ * 可假定整个链表结构中没有循环。
+ * 程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/intersection-of-two-linked-lists
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+class P160 {
+    fun getIntersectionNode(headA: ListNode?, headB: ListNode?): ListNode? {
+        return null
+    }
+}
